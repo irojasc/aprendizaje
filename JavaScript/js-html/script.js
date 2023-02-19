6b167eed5f884c1b2019b72981ba6d012053d69d
genesis_sntg <libreriagenesisstc@gmail.com>
2023-02-19 18:21:00 -0500

    Se agrega el contenido de la conexion de los eventos html en js

--- JavaScript/js-html/script.js
+++ JavaScript/js-html/script.js
@@ -9,20 +9,22 @@ const p = document.querySelectorAll('p'); //este selecciona solo al primer selec
 //const p = document.querySelectorAll('p'); //este selecciona solo todos los selectores p
 const parrafito = document.querySelector('.parrafito');
 const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('input');
+const input2 = document.querySelector('#input2-calculo');
+const btnCalcular = document.querySelector("btn-calcular");
 
-console.log(input.value); //
+console.log(input1.value); //
 
 console.log({
-    h1, p, parrafito, pid, input}
+    h1, p, parrafito, pid, input1}
 );
 
 /* (vvvvvvvv) Esto sirve para setear los valores textuales de un elemento html */
 h1.innerHTML = "Rios profundos  <br> de Jose Maria Arguedas"; //Este cambia el valor del texto, tambien se puede incluir codigo HTML
 h2.innerText = "Rios profundos <br> de Jose Maria Arguedas"; //Este cambia el valor netamente textual
 //****para el caso de modficar un atributo value para input*/
-input.value = "";
-input.placeholder = "Hola causita";
+input1.value = ""; //en esta parte se eliminar el contenido del input
+input1.placeholder = "Hola causita";
 
 /* (vvvvvvvv) Esto sirve para LEER valores de atributos y contenido textual */
 
@@ -49,6 +51,13 @@ pid.innerHTML = ""; //esto lo que hace es borrar el contenido html del elemento
 pid.append(img); // agrega el elemento <img> dentro del elemento con id = "pid"
 //pid.appendChild //tambien se puede utilizar esto
 
+
+//En esta parte se ve el ejemplo de calculadora 
+//para pasar valores strings a numero: Number(), parseInt(), colocar + delante el input
+function btnOnClick(){
+    const resultado = +input1.value + +input2.value;
+    h1.innerText = "Resultado: " + resultado;
+}
 
 
 
